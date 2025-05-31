bda3ba49c5fc71079669e266d643180f757029a1
Thisara Dilshan <it21067938@my.sliit.lk>
2025-05-31 21:25:59 +0530

    Fix the nav

--- resources/js/Components/Hero/Hero.tsx
+++ resources/js/Components/Hero/Hero.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
-  return (
-    <div>
-        Welcome to Sri Lanka
-See what's waiting for you on your next island getaway.
-Savour the unique experiences this island treasure has to offer.</div>
-  )
+    return (
+        <div>
+          
+            <p className="text-4xl font-serif italic font-light p-10">Welcome to Sri Lanka</p>
+            See what's waiting for you on your next island
+            getaway. Savour the unique experiences this island treasure has to
+            offer.
+        </div>
+    );
 }
 
-export default Hero
+export default Hero;
--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -18,9 +18,9 @@ export default function Authenticated({
         useState(false);
 
     return (
-        <div className="flex flex-col min-h-screen bg-gray-100">
+        <div className="flex flex-col min-h-screen bg-gray-100 ">
             <ToastContainer/>
-            <nav className="border-b border-gray-100 bg-white">
+            <nav className="fixed top-0 left-0 right-0 z-10 border-b border-gray-100 bg-white">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="flex h-20 justify-between">
                         <div className="flex">
